90cd4c324639032a8c1168d5a3933bfe82a2f7cd
sam0705w <79701050+sam0705w@users.noreply.github.com>
2021-09-14 17:48:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,15 +12,23 @@ recognition.onresult=function run (event)//John.onListened
     console.log(event);
     var content= event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML= content
-    speak()
+    if(content=="take my selfie"){
+speak()
+    }
+   
 }
 
 function speak(){
     var start= window.speechSynthesis
-    var word=document.getElementById("textbox").value
+    var word= "take my selfie in 5 seconds"
     var speech= new SpeechSynthesisUtterance(word)
     start.speak(speech)
     Webcam.attach(camera)
+
+    setTimeout(function(){
+        snapshot()
+        download()
+    },5000)
 }
 Webcam.set({
     width: 360,
@@ -30,3 +38,18 @@ Webcam.set({
 }
 )
 var camera=document.getElementById("camera")
+
+function snapshot(){
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML="<img id='selfie_image' src="+data_uri+">"
+})
+
+}
+function download(){
+link=document.getElementById("link")//refering to the html anchor tag
+image=document.getElementById("selfie_image").src//refering to the snapshot image src file
+link.href=image
+link.click()
+console.log(link)
+console.log(image)
+}
